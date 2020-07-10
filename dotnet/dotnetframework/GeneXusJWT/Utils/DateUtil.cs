5ae3faf9fc6dab21cea1807cd34520339d03290c
sgrampone <30872877+sgrampone@users.noreply.github.com>
2020-07-10 15:54:20 -0300

    Add JWT DateUtil functions (#24)

--- dotnet/dotnetframework/GeneXusJWT/Utils/DateUtil.cs
+++ dotnet/dotnetframework/GeneXusJWT/Utils/DateUtil.cs
@@ -1,12 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Security;
 using System.Globalization;
 using GeneXusJWT.GenexusComons;
-using SecurityAPICommons.Commons;
+
 
 namespace GeneXusJWT.GenexusJWTUtils
 {
@@ -41,6 +37,65 @@ namespace GeneXusJWT.GenexusJWTUtils
         {
             DateTime date = DateTime.ParseExact(DateTime.UtcNow.AddSeconds(seconds).ToString("yyyy/MM/dd HH:mm:ss"), "yyyy/MM/dd HH:mm:ss", CultureInfo.InvariantCulture);
             return date.ToString("yyyy/MM/dd HH:mm:ss");
+        }
+
+        [SecuritySafeCritical]
+        public string CurrentPlusMinutes(long minutes)
+        {
+            DateTime date = DateTime.ParseExact(DateTime.UtcNow.AddMinutes(minutes).ToString("yyyy/MM/dd HH:mm:ss"), "yyyy/MM/dd HH:mm:ss", CultureInfo.InvariantCulture);
+            return date.ToString("yyyy/MM/dd HH:mm:ss");
+        }
+
+        [SecuritySafeCritical]
+        public string CurrentPlusHours(long hours)
+        {
+            DateTime date = DateTime.ParseExact(DateTime.UtcNow.AddHours(hours).ToString("yyyy/MM/dd HH:mm:ss"), "yyyy/MM/dd HH:mm:ss", CultureInfo.InvariantCulture);
+            return date.ToString("yyyy/MM/dd HH:mm:ss");
+        }
+
+        [SecuritySafeCritical]
+        public string CurrentPlusDays(long days)
+        {
+            DateTime date = DateTime.ParseExact(DateTime.UtcNow.AddDays(days).ToString("yyyy/MM/dd HH:mm:ss"), "yyyy/MM/dd HH:mm:ss", CultureInfo.InvariantCulture);
+            return date.ToString("yyyy/MM/dd HH:mm:ss");
+        }
+
+        [SecuritySafeCritical]
+        public string CurrentPlusMonths(int months)
+        {
+            DateTime date = DateTime.ParseExact(DateTime.UtcNow.AddMonths(months).ToString("yyyy/MM/dd HH:mm:ss"), "yyyy/MM/dd HH:mm:ss", CultureInfo.InvariantCulture);
+            return date.ToString("yyyy/MM/dd HH:mm:ss");
+        }
+        [SecuritySafeCritical]
+        public string LastDayOfCurrentMonth(string time)
+        {
+            DateTime date;
+            try
+            {
+                date = DateTime.ParseExact(time, "HH:mm:ss", CultureInfo.InvariantCulture);
+            }
+            catch (Exception)
+            {
+                this.error.setError("DU001", "Wrong format in input parameter");
+                return "";
+            }
+            int hour = date.Hour;
+            int minutes = date.Minute;
+            int seconds = date.Second;
+            int day = DateTime.DaysInMonth(DateTime.UtcNow.Year, DateTime.UtcNow.Month);
+            int year = DateTime.UtcNow.Year;
+            int month = DateTime.UtcNow.Month;
+            String result;
+            try
+            {
+                result = String.Format("{0}/{1:D2}/{2:D2} {3:D2}:{4:D2}:{5:D2}", year, month, day, hour, minutes, seconds);
+            }
+            catch (Exception)
+            {
+                this.error.setError("DU002", "Could not generate correct date");
+                return "";
+            }
+            return result;
         }
 
         /******** EXTERNAL OBJECT PUBLIC METHODS - END ********/
