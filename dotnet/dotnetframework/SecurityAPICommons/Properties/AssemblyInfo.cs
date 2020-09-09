211d630b936ffd519d67feb70e1413aca2da184f
sgrampone <30872877+sgrampone@users.noreply.github.com>
2020-09-09 10:58:24 -0300

    Update binaries version (#35)

--- dotnet/dotnetframework/SecurityAPICommons/Properties/AssemblyInfo.cs
+++ dotnet/dotnetframework/SecurityAPICommons/Properties/AssemblyInfo.cs
@@ -32,7 +32,7 @@ using System.Security;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("16.11.0")]
-[assembly: AssemblyFileVersion("16.11.0")]
+[assembly: AssemblyVersion("17.0.0")]
+[assembly: AssemblyFileVersion("17.0.0")]
 [assembly: AllowPartiallyTrustedCallers]
 
